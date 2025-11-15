cec1b6d48db72b0adde6369fb157278e48831fe6
RACHAMALL <71813900+RACHAMALL@users.noreply.github.com>
2025-11-15 14:39:22 +0530

    practice4.sh

---
+++ practice3.sh
@@ -0,0 +1,7 @@
+#!bin/bash  # here it is called shebang in the linx below command will execute using bin directory  
+A=Sai #here after no space same after = no space 
+B=Narasimha
+echo "$A :: hi $B"
+echo "$B :: hi $A"
+echo "$A :: rent paycheye"
+echo "$B :: konchem time ivvu"
